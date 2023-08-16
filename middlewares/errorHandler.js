fa23011c20104e240362c4553e219a6e72eee1ed
sergiopaulodev <101745880+sergiopaulodev@users.noreply.github.com>
2023-08-16 19:52:07 -0300

    init-queries (#3)

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -6,11 +6,4 @@ export default function(err, res) {
         response: null
     })
 
-// //     // set locals, only providing error in development
-// //   res.locals.message = err.message;
-// //   res.locals.error = req.app.get('env') === 'development' ? err : {};
-                       
-// //   // render the error page
-// //   res.status(err.status || 500);
-// //   res.render('error');
 }
