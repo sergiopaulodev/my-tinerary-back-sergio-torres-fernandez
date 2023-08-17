05b1352a7112b51abfeb04d524ed4d85b3cc00d1
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-08-16 22:00:37 -0300

    filter

--- controllers/cities/read.js
+++ controllers/cities/read.js
@@ -2,13 +2,46 @@ import City from '../../models/City.js';
 
 export default async (req, res, next) => {
     try {
+        
+        // console.log(req.query);
+        
+        let search = {}
+        let order = {}
+        
+        if (req.query.admin_id){
+            search.admin_id = req.query.admin_id
+        }
+        
+        if (req.query.sort){
+            order.city = req.query.sort
+        }
 
-        let allCities = await City.find()
-        return res.status(200).json({
-            succes: true,
-            message: 'cities found',
-            response: allCities
-        })
+        if (req.query.city){
+            search.city = new RegExp(req.query.city, 'i')
+        }
+
+        // // new RegExp(req.query.title, 'i')
+        // // let allCities = await City.find().select('country city photo smalldescription admin_id')
+
+        let allCities = await City
+        .find(search, 'country city photo smalldescription admin_id')
+        .populate('admin_id', 'photo mail name')
+        .sort(order)
+        
+        if (allCities.length > 0){
+            return res.status(200).json({
+                succes: true,
+                message: 'cities found',
+                response: allCities
+            })
+        } else {
+            return res.status(404).json({
+                success: false,
+                message: 'not found',
+                response: null
+            })
+
+        }
         
     } catch (error) {
         next(error);
