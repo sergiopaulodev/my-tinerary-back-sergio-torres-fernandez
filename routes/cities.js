4c392bc2d79a3e7628ed100c0febbc020e1998ec
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-08-29 11:35:41 -0300

    itineraries

--- routes/cities.js
+++ routes/cities.js
@@ -1,19 +1,17 @@
 import express from 'express';
-let router = express.Router();
 import create from '../controllers/cities/create.js';
 import read from '../controllers/cities/read.js';
 import readOne from '../controllers/cities/readOne.js';
 import update from '../controllers/cities/update.js';
 import destroy from '../controllers/cities/destroy.js';
 import carousel from '../controllers/cities/carousel.js';
-import cityDetails from '../controllers/cities/cityDetails.js'
+let router = express.Router();
 
 
 
 router.post('/', create)
 router.get('/', read)
 router.get('/carousel', carousel)
-router.get('/citydetails/:_id', cityDetails)
 router.get('/:_id', readOne)
 router.put('/:u_id', update)
 router.delete('/:id', destroy)
