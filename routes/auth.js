f553e60d39dd4cc3ff1186ac88df9c5afd19b2ec
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-09-05 10:09:28 -0300

    middlewares-validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
 import register from "../controllers/auth/register.js";
 
+import isValidPass from "../middlewares/isValidPass.js";
+import existsUser from "../middlewares/existsUser.js";
+import validator from "../middlewares/validator.js";
+import registerSchema from "../schemas/register.js";
+
+import read from "../controllers/users/read.js";
+// import readOne from "../controllers/users/readOne.js";
+// import update from "../controllers/users/update.js";
+import destroy from "../controllers/users/destroy.js";
+
 const authRouter = Router()
 
 // // authRouter.post('/signup', register)
@@ -8,6 +18,15 @@ const authRouter = Router()
 //VALIDAR datos con joi
 //validar que la cuenta NO existe para que no haya doble registe
 //hashear la contrase;a
-authRouter.post('/register', register)  
+
+
+authRouter.post('/register', validator(registerSchema), existsUser, isValidPass, register)
+// validator(registerSchema),existsUser, isValidPass,
+
+
+authRouter.get('/', read)
+// authRouter.get('/:id', readOne)
+// authRouter.put('/:u_id', update)
+authRouter.delete('/:id', destroy)
 
 export default authRouter
---
+++ schemas/register.js
@@ -0,0 +1,22 @@
+import joi from "joi";
+
+let registerSchema = joi.object({
+   //obligatorio
+    name:joi.string().required().min(3).max(20).messages({
+        "string.min":"name must have at least 3 characters please",
+        "string.max":"name must be less than 21 characters please",
+        "any.required": "name is required", //cuando no se envia el dato
+        "string.empty": "name is required" // cuando se envie vacio
+    }),
+    mail:joi.string().required(),
+    password: joi.required(),
+    country: joi.required(),
+    //no obligatorio
+    photo: joi.string().required().uri(),
+    lastname: joi.string().min(3).max(20).empty('').messages({
+     "string.min":"name must have at least 3 characters please",
+     "string.max":"name must be less than 21 characters please"
+    })
+})
+
+export default registerSchema
