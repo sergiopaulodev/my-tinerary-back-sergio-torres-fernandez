36790f7f3f139b0319b12d6666bea4bf2e966fc8
sergiopaulodev <101745880+sergiopaulodev@users.noreply.github.com>
2023-09-12 21:55:40 -0300

    Login (#11)
    * login
    
    * passport
    
    * activities
    
    * activities-2
    
    * register-fixed

--- routes/auth.js
+++ routes/auth.js
@@ -16,19 +16,12 @@ import validator from "../middlewares/validator.js";
 import registerSchema from "../schemas/register.js";
 import signInSchema from "../schemas/signin.js";
 
-// import readOne from "../controllers/users/readOne.js";
-// import update from "../controllers/users/update.js";
 import read from "../controllers/users/read.js";
-// import destroy from "../controllers/users/destroy.js";
+
 
 
 const authRouter = Router()
 
-// // authRouter.post('/signup', register)
-// //register requiere middlewares para VALIDAR/VERIFICAR/AUTENTICAR
-// //VALIDAR datos con joi
-// //validar que la cuenta NO existe para que no haya doble registe
-// //hashear la contrase;a
 
 
 authRouter.post(
@@ -66,8 +59,7 @@ authRouter.post(
 
 
 authRouter.get('/', read)
-// authRouter.get('/:id', readOne)
-// authRouter.put('/:u_id', update)
-// authRouter.delete('/:id', destroy)
+
+
 
 export default authRouter;
